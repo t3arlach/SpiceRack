2e158a6c9c3204729dff53d2013b832446d633ac
t3arlach <brian.stam@gmail.com>
2013-11-24 16:22:23 -0500

    Added custom launcher icon

--- gen/com/stam/spicerack/R.java
+++ gen/com/stam/spicerack/R.java
@@ -24,6 +24,7 @@ public final class R {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_action_star=0x7f020001;
         public static final int ic_launcher=0x7f020002;
+        public static final int ic_spicerack_launcher=0x7f020003;
     }
     public static final class id {
         public static final int action_about=0x7f0a0012;
